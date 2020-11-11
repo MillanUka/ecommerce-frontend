0d8e0a334b36da031b3b257e2a947dca4c89fd42
MillanUka <ukamillan@gmail.com>
2020-11-11 17:08:21 +1300

    Add basic homepage that displays the products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    AppRoutingModule,
     HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    BrowserAnimationsModule,
   ],
-  declarations: [
-    AppComponent,
-  ],
-  bootstrap: [ AppComponent ]
+  declarations: [AppComponent, HomeComponent, AboutComponent],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
